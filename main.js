f0515cc7dd719b9486ca25dd2a361ade8f19e4e8
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-14 15:35:07 -0400

    Figures out how to convert address input to url code

--- main.js
+++ main.js
@@ -8,8 +8,14 @@ const searchButton = document.querySelector("#search");
 //add event listener to button
 searchButton.addEventListener("click", function(){
    //log out the .values of the input fields
-    console.log("api input: " + apiInput.value);
+    /*console.log("api input: " + apiInput.value);
     console.log("address input: " + addressInput.value);
-    console.log("norad input: " + noradInput.value); 
+    console.log("norad input: " + noradInput.value);*/ 
+
+    //will convert the address input with spaces to have %20 for url purposes
+    let addressString = addressInput.value;
+    addressString = encodeURIComponent(addressString.trim())
+    
+    console.log("New address input: " + addressString);    
 })
 
