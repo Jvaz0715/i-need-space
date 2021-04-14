2287f80a61d6f745f284a58d92f48057d4fd775d
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-14 19:40:51 -0400

    makes.then that arrays coordinates

--- main.js
+++ main.js
@@ -18,7 +18,25 @@ searchButton.addEventListener("click", function(){
     
     console.log("New address input: " + addressString);
     
-    const URL = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + addressString + ".json?access_token=" + apiInput.value;
-    console.log(URL);
+    const mapURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/" + addressString + ".json?access_token=" + apiInput.value;
+    console.log(mapURL);
+    fetch(mapURL)
+    // Now process the raw response into an object
+    .then((res) => res.json())
+    // Now process the above JSON, we will first console log a message
+    //but we want to use this space to change the img src
+    .then((data) => {
+        // declare the coordinates of each latitude and longitude
+        const latitude = data.features[0].geometry.coordinates[0];
+        const longitude = data.features[0].geometry.coordinates[1];
+        // test the latitude + longitude variables by console logging them
+        console.log("This is latitude: " + latitude);
+        console.log("This is longitude: " + longitude);
+        const coordinates = [latitude, longitude];
+        return coordinates;
+    })
+    .then((coordinates) => {
+        console.log(coordinates)
+    })
 })
 
