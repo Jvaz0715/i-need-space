f023ec38feb174d25644fc8274d4c50cb7bb13cf
Jvaz0715 <james.vazquez@codeimmersives.com>
2021-04-15 13:15:46 -0400

    Converts to local date and time

--- main.js
+++ main.js
@@ -5,6 +5,14 @@ const noradInput = document.querySelector("#norad");
 //query the search button
 const searchButton = document.querySelector("#search");
 
+//some helper functions
+//to convert month number to month name
+// function changeToMonth(num) {
+    
+    
+// }
+//to convert utc data to Month DD, YYYY
+
 //add event listener to button
 searchButton.addEventListener("click", function(){
     //will convert the address input with spaces to have %20 for url purposes
@@ -39,18 +47,40 @@ searchButton.addEventListener("click", function(){
             // Now process the above JSON
             .then((data) => {
             // test to make sure you can log out the satellite url properply
-            console.log('If this succeeds, than the sat url should be returned below: ')
-            console.log(satAPIurl);
-            // test to target data information
-            console.log('If this succeeds, than the data will show here: ')
-            console.log(data);
-            //check for rise information
-            console.log("Rise: " + data[0].rise.utc_datetime);
-            //check for culmination information
-            console.log("Culminates: " +data[0].culmination.utc_datetime);
-            //check for set information
-            console.log("Sets: " + data[0].set.utc_datetime);
-            
+            // console.log('If this succeeds, than the sat url should be returned below: ')
+            // console.log(satAPIurl);
+            // // test to target data information
+            // console.log('If this succeeds, than the data will show here: ')
+            // console.log(data);
+            // //check for rise information
+            // console.log("Rise: " + data[0].rise.utc_datetime);
+            // //check for culmination information
+            // console.log("Culminates: " +data[0].culmination.utc_datetime);
+            // //check for set information
+            // console.log("Sets: " + data[0].set.utc_datetime);
+
+            // Gets the date on the left, space, gmt time on the right
+            // let dataArray = new Array();
+            // dataArray = data[0].rise.utc_datetime.split(".");
+            // const date
+            // console.log("Data Array: " + dataArray[0]);
+
+            console.log(data[0].rise.utc_datetime)
+            const dateUTC = data[0].rise.utc_datetime //+ " UTC";
+            console.log(dateUTC);
+            const localDate = new Date(dateUTC);
+
+            console.log("Local Date is: " + localDate);
+            // Use this to create a count down later on
+            // const currentdate = new Date(); 
+            // const datetime = "Last Sync: " + currentdate.getDate() + "/"
+            //     + (currentdate.getMonth()+1)  + "/" 
+            //     + currentdate.getFullYear() + " @ "  
+            //     + currentdate.getHours() + ":"  
+            //     + currentdate.getMinutes() + ":" 
+            //     + currentdate.getSeconds(); 
+
+            // console.log(currentdate);
       
     })
     })
